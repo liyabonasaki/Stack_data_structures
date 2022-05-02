12dde970b7606e25268e36ad185d6b934d5c0b32
Liyabona Saki <liyabonasaki@gmail.com>
2022-05-02 12:23:40 +0200

    adding ignore file and comments in code

--- src/cput/ac/za/Main.java
+++ src/cput/ac/za/Main.java
@@ -8,18 +8,20 @@ package cput.ac.za;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
         Stack st = new Stack();
 
+        //pushing elements to the stack
         st.push(5);
         st.push(10);
         st.push(8);
         st.push(18);
         st.push(20);
 
+        //popping first element from the stack
+		System.out.println("Pop : " + st.pop());
 
-//		System.out.println("Pop : " + st.pop());
-
+		//selecting first element from the stack
         System.out.println("Peek : " + st.peek());
 
         st.show();
